69683e898c4777bde9eed0e99e985767999b8d99
Evgeniy Pelmenev <g.snake345@gmail.com>
2016-07-18 22:19:39 +0200

    fix strike element

---
+++ output/safari/versiononeenhancements_1.2.3.safariextension/ckeditor/plugins/simpleuploads/lang/zh-cn.js
@@ -0,0 +1,41 @@
+﻿CKEDITOR.plugins.setLang( 'simpleuploads', 'zh-cn',
+	{
+		// Tooltip for the "add file" toolbar button
+		addFile : '添加文件',
+		// Tooltip for the "add image" toolbar button
+		addImage: '添加图像',
+
+		// The server returned a URL after the upload that it's wrong
+		badUrl: 'Failed to load the image with the provided URL',
+
+		// Generic error for http status codes. Specific ones can be defined as 'httpStatus400', etc...
+		errorPostFile: 'Error posting the file to %0\r\nResponse status:',
+
+		// File size is over config.simpleuploads_maxFileSize OR the server returns HTTP status 413
+		fileTooBig : '文件太大，请使用较小的文件。',
+
+		// The height of the image is over the allowed maximum
+		imageTooTall: 'The image is too tall',
+
+		// The width of the image is over the allowed maximum
+		imageTooWide: 'The image is too wide',
+
+		// The extension matches one of the blacklisted ones in config.simpleuploads_invalidExtensions
+		invalidExtension : '文件类型无效，请只使用有效的文件。',
+
+		// The extension isn't included in config.simpleuploads_acceptedExtensions
+		nonAcceptedExtension: '文件类型无效，请只使用有效的文件:\r\n%0',
+
+		// The file isn't an accepted type for images
+		nonImageExtension: 'You must select an image',
+
+		// Shown after the data has been sent to the server and we're waiting for the response
+		processing: '正在处理…',
+
+		// images with webkit-fake-url can't be read
+		uselessSafari: 'Sorry, the images pasted with Safari aren\'t usable',
+
+		// Error in the XHR upload. I'm not sure under which conditions this could happen, but it's a safety check
+		xhrError: 'Error posting the file to %0'
+	}
+);
---
+++ output/safari/versiononeenhancements_1.2.3.safariextension/content.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @include *://*.v1host.com/*
+// @require injectHelpers.js
+// ==/UserScript==
+var defaultOptions = {
+	replaceWysiwyg: true,
+	minHeightWysiwyg: 200,
+	maxHeightWysiwyg: 0,
+	typography: true,
+	removeExtraTags: true,
+	expand: true,
+	myWorkEnhancement: true
+};
+var options;
+
+kango.addMessageListener('OptionsToContent', function(event) {
+	kango.storage.setItem('options', event.data);
+});
+
+
+options = kango.storage.getItem('options');
+options = extend(defaultOptions, options ? options : {});
+options.baseUrl = kango.io.getResourceUrl('content.js').replace('content.js', '');
+
+injectScript(kango.io.getResourceUrl('injectHelpers.js'), function() {
+	if (options.replaceWysiwyg) {
+		injectScript(kango.io.getResourceUrl('replaceWysiwyg.js'), sentOptions);
+	}
+
+	if (options.typography) {
+		injectScript(kango.io.getResourceUrl('typography.js'), sentOptions);
+	}
+
+	if (options.expand) {
+		injectStyle(kango.io.getResourceUrl('expand.css'));
+	}
+
+	if (options.myWorkEnhancement) {
+		injectScript(kango.io.getResourceUrl('myWork.js'), sentOptions);
+	}
+});
+
+function sentOptions() {
+	var customEvent = new CustomEvent('SentOptions', {
+		detail: options
+	});
+	window.dispatchEvent(customEvent);
+}
+
+function extend(obj, targetObj) {
+	for (var i in targetObj) {
+		if (targetObj.hasOwnProperty(i)) {
+			obj[i] = targetObj[i];
+		}
+	}
+
+	return obj;
+}
